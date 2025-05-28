e01cfe6bacbe1a490dd9c46186bc65f72cd4a53d
Woowonjae <1056356463@qq.com>
2025-05-28 16:06:16 +0800

    有问题5

--- src/components/album/AlbumCard.tsx
+++ src/components/album/AlbumCard.tsx
@@ -11,7 +11,7 @@ const AlbumCard: FC<AlbumCardProps> = ({ album, onPlay }) => {
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden">
       <div className="h-[300px]">
-        <ThreeJSAlbumRenderer imageSrc={album.imageSrc} />
+        <ThreeJSAlbumRenderer imageSrc={album.coverUrl} />
       </div>
       <div className="p-4">
         <h3 className="text-xl font-semibold">{album.title}</h3>
